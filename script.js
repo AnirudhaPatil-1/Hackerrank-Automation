8b47348160395c68c15c9ceca1a387188fe9af1a
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-01-25 21:41:42 +0530

    progess: collected url of each problem on page day 1

--- script.js
+++ script.js
@@ -1,16 +1,49 @@
 const pup = require("puppeteer");
+let id = "seyavor657@fandua.com";
+let pass = "seyavor657@fandua.com";
 
 let browserPromise = pup.launch({
     headless: false,
     defaultViewport: false
 });
-console.log(browserPromise);
+// console.log(browserPromise);
 //OUTPUT -> Promise { <pending> }
+let tab;
+browserPromise.then(function(browser){
+    let pagesPromise = browser.pages();
+    return pagesPromise;
+}).then(function(pages){
+    tab = pages[0];
+    let pageOpenPromise = tab.goto("https://www.hackerrank.com/auth/login");
+    return pageOpenPromise;
+}).then(function(){
+    let idPromise = tab.type('#input-1',id);
+    return idPromise;
+}).then(function(){
+    let passPromise = tab.type('#input-2',pass);
+    return passPromise;
+}).then(function(){
+    let  loginPromise = tab.click(".ui-btn.ui-btn-large.ui-btn-primary.auth-button.ui-btn-styled");
+    return loginPromise;
+}).then(function(){
+    let waitPromise =  tab.waitForSelector(".ui-btn.ui-btn-normal.ui-btn-primary.recommended-prep-kit-card-cta.ui-btn-link.ui-btn-styled", {visible: true});
+    return waitPromise;
+}).then(function(){
+    let IpkClickPromise = tab.click(".ui-btn.ui-btn-normal.ui-btn-primary.recommended-prep-kit-card-cta.ui-btn-link.ui-btn-styled");
+    return IpkClickPromise;
+}).then(function(){
+    let waitPromise = tab.waitForSelector(".ui-btn.ui-btn-normal.ui-btn-line-primary.interview-ch-li-cta.ui-btn-link.ui-btn-styled", {visible: true});
+    return waitPromise;
+}).then(function(){
+    // console.log("warm up challenges");
+}).then(function(){
+    let allUrlsPromise = tab.$$(".ui-btn.ui-btn-normal.ui-btn-line-primary.interview-ch-li-cta.ui-btn-link.ui-btn-styled");
+    return allUrlsPromise;
+}).then(function(data){
+    console.log(data.length);   
+}).then(function(){
+    console.log("CODE WORKING");
+}).catch(function(err){
+    console.log(`ERROR OCCURED ${err}`);
+})
 
-// browserPromise.then(function(browser){
-//     let pagesPromise = browser.pages();
-//     return pagesPromise;
-// }).then(function(pages){
-//     tab = pages[0];
-//     tab.goto("https://www.hackerrank.com/auth/login");
-// })
