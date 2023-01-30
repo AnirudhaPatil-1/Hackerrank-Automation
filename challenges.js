7424734c60ceeb2ccd19a725dec900ef7a47d9b8
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-01-30 21:41:46 +0530

    add the pepchallenges file containing content for challenge creation and udpate code accordingly in challenges.js file

--- challenges.js
+++ challenges.js
@@ -1,7 +1,9 @@
 const { linkSync } = require("fs");
 const pup = require("puppeteer");
+const { elementIsDisabled } = require("selenium-webdriver/lib/until");
 let id = "seyavor657@fandua.com";
 let pass = "seyavor657@fandua.com";
+let challenges = require("./pepchallenge"); 
 
 async function main(){
     let browser = await pup.launch({
@@ -18,15 +20,24 @@ async function main(){
     await tab.click(".ui-btn.ui-btn-large.ui-btn-primary.auth-button.ui-btn-styled");
     await tab.waitForNavigation({waitUntil: "networkidle2"});
     await tab.click(".dropdown-handle.nav_link.toggle-wrap");
-    await tab.waitForNavigation({waitUntil: "networkidle2"});
+    // await tab.waitForNavigation({waitUntil: "networkidle2"});
     // await tab.waitForSelector("a[data-analytics='NavBarProfileDropDownAdministration']", {visible: true})
     await tab.click("a[data-analytics='NavBarProfileDropDownAdministration']");
-    await tab.waitForNavigation({waitUntil: "networkidle2"});
+    // await tab.waitForNavigation({waitUntil: "networkidle2"});
     await tab.waitForSelector(".nav-tabs.nav.admin-tabbed-nav li", {visible: true}); 
     let linkLists = await tab.$$(".nav-tabs.nav.admin-tabbed-nav li");
     await linkLists[1].click();
     await tab.waitForSelector(".btn.btn-green.backbone.pull-right", {visible: true});
-    await tab.click(".btn.btn-green.backbone.pull-right");
+    // await tab.click(".btn.btn-green.backbone.pull-right");
+    let createChallengeButton = await tab.$(".btn.btn-green.backbone.pull-right");
+    let createChallengeUrl = await tab.evaluate(function(ele){
+        ele.getAttribute("href");
+    }, createChallengeButton)
+    console.log(createChallengeUrl);
+}
+
+async function createChallenge(){
 
 }
+
 main();
