36726dc9082ab6fe51eb8e9e6aa4384b69867cb2
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-01-31 15:41:39 +0530

    Project completed. Async code format. 11 Challenges created using Automation

--- challenges.js
+++ challenges.js
@@ -1,17 +1,15 @@
-const { linkSync } = require("fs");
 const pup = require("puppeteer");
-const { elementIsDisabled } = require("selenium-webdriver/lib/until");
-let id = "seyavor657@fandua.com";
-let pass = "seyavor657@fandua.com";
-let challenges = require("./pepchallenge"); 
-
-async function main(){
+// let id = "pokowa1759@shzsedu.com";
+// let pass = "Random@1997";
+let id = "lilaki8723@brandoza.com";
+let pass = "randompassword123"
+let challenges = require("./pepchallenge");
+async function main() {
     let browser = await pup.launch({
         headless: false,
         defaultViewport: false,
         args: ["--start-maximized"]
-    })
-
+    });
     let pages = await browser.pages();
     let tab = pages[0];
     await tab.goto("https://www.hackerrank.com/auth/login");
@@ -20,24 +18,36 @@ async function main(){
     await tab.click(".ui-btn.ui-btn-large.ui-btn-primary.auth-button.ui-btn-styled");
     await tab.waitForNavigation({waitUntil: "networkidle2"});
     await tab.click(".dropdown-handle.nav_link.toggle-wrap");
-    // await tab.waitForNavigation({waitUntil: "networkidle2"});
-    // await tab.waitForSelector("a[data-analytics='NavBarProfileDropDownAdministration']", {visible: true})
     await tab.click("a[data-analytics='NavBarProfileDropDownAdministration']");
-    // await tab.waitForNavigation({waitUntil: "networkidle2"});
-    await tab.waitForSelector(".nav-tabs.nav.admin-tabbed-nav li", {visible: true}); 
+    await tab.waitForSelector(".nav-tabs.nav.admin-tabbed-nav li", {visible: true});
     let linkLists = await tab.$$(".nav-tabs.nav.admin-tabbed-nav li");
     await linkLists[1].click();
     await tab.waitForSelector(".btn.btn-green.backbone.pull-right", {visible: true});
-    // await tab.click(".btn.btn-green.backbone.pull-right");
     let createChallengeButton = await tab.$(".btn.btn-green.backbone.pull-right");
     let createChallengeUrl = await tab.evaluate(function(ele){
-        ele.getAttribute("href");
-    }, createChallengeButton)
-    console.log(createChallengeUrl);
+        return ele.getAttribute("href");
+    },createChallengeButton);
+    for(let i = 0; i < challenges.length; i++) {
+        await createChallenge("https://www.hackerrank.com" + createChallengeUrl,challenges[i],await browser.newPage() );
+    }
+    
+    
+
 }
 
-async function createChallenge(){
-
+async function createChallenge(url,challenge,tab) {
+    await tab.goto(url);
+    await tab.waitForSelector("#name",{visible: true});
+    await tab.type("#name",challenge["Challenge Name"]);
+    await tab.type("#preview",challenge["Description"]);
+    await tab.waitForSelector("#problem_statement-container .CodeMirror textarea",{visible: true});
+    await tab.type("#problem_statement-container .CodeMirror textarea", challenge["Problem Statement"]);
+    await tab.type("#input_format-container .CodeMirror textarea", challenge["Input Format"]);
+    await tab.type("#constraints-container .CodeMirror textarea", challenge["Constraints"]);
+    await tab.type("#output_format-container .CodeMirror textarea", challenge["Output Format"]);
+    await tab.type("#tags_tag",challenge["Tags"]);
+    await tab.keyboard.press("Enter");
+    await tab.click(".save-challenge.btn.btn-green");
 }
-
 main();
+/********************************************************** */
